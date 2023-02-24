13e67def937f84de8684512a7e67a3339fa0b501
Faith <faithchemutai2330@gmail.com>
2023-02-24 12:51:56 +0300

    added migrate and post requests

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -5,4 +5,25 @@ class TodoController < Sinatra::Base
         "Our very first sinatra"
     end
 
+    post '/todos/create' do #(adding data using a post request)
+         data = JSON.parse (request.body.read)
+         
+        begin
+         # approach 1 (individual columns)
+         today = Time.now
+        #  title = data["title"]
+        #  description = data["description"]
+        #  todo = Todo.create(title: title, description: descrption, createdAt: today)
+        #  todo.to_json
+
+         #approach 2 (hash or column)
+         data["createdAt"] = today
+         todo = Todo.create(data)
+         todo.to_json
+        rescue => e
+            {
+                error: "an error ocurres while creating a new to do"
+            }.to_json
+        end
+end
 end
--- app/models/todos.rb
+++ app/models/todos.rb
@@ -1,3 +1,4 @@
 #class inherits from the ActiveRecord::base class which provides it with a number of methods for interacting with a database.
 class Todo < ActiveRecord::Base
+    enum :status, [ :CREATED, :ONGOING, :COMPLETED, :CANCELLED ]
 end
